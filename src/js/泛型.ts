ce164ebe931ced1df2e04fae8d74b8470534766a
XingZheMonkey <50906069+XingZheMonkey@users.noreply.github.com>
2021-06-17 14:50:37 +0800

    Update 泛型.ts

--- src/js/泛型.ts
+++ src/js/泛型.ts
@@ -20,9 +20,61 @@
         (x: T, y: T): T
     }
 
-    let add: Iadd<number> = function (x: number, y: number) {
+    let add: Iadd<number> = function (x, y) {
         return x + y;
     }
 
     console.log(add(3, 4));
+
+
+    // 多个泛型参数
+    function getMsg<K, V>(val1: K, val2: V): [K, V] {
+        return [val1, val2];
+    }
+
+    console.log(getMsg<number, string>(22, 'henry'));
+
+
+    // 泛型约束
+    interface ILength {
+        length: number
+    }
+
+    function getLength<T extends ILength>(x: T): number {
+        return x.length
+    }
+
+    console.log(getLength<string>("abcsefg"))
+
+
+    // 泛型示例
+    interface IBaseCURD<T> {
+        data: Array<T>
+        add: (t: T) => T
+        getUserId: (id: number) => T
+    }
+
+    class User {
+        id?: number
+        name: string
+        age: number
+    }
+
+    class UserCURD implements IBaseCURD<User>{
+        data: Array<User> = []
+        add(user: User): User {
+            user.id = Date.now() + Number(Math.random().toFixed(0));
+            this.data.push(user)
+            return user
+        }
+        getUserId(id: number): User {
+            return this.data.find(user => user.id == id)
+        }
+    }
+
+    let henry = new UserCURD()
+    henry.add({name:'henry',age:24})
+    henry.add({name:'marry',age:24})
+    console.log(henry.data)
+
 })()
